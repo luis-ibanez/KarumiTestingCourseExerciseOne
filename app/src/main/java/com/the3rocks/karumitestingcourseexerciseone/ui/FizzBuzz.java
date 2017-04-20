2fa9daebe6b8ac8cb1f7f2d4923a2013fb319e70
Luis Ibanez <luis@the3rocks.com>
2017-04-20 19:01:37 +0200

    Added constants

--- app/src/main/java/com/the3rocks/karumitestingcourseexerciseone/ui/FizzBuzz.java
+++ app/src/main/java/com/the3rocks/karumitestingcourseexerciseone/ui/FizzBuzz.java
@@ -3,14 +3,18 @@ package com.the3rocks.karumitestingcourseexerciseone.ui;
 
 public class FizzBuzz {
 
+    public static final String FIZZ = "fizz";
+    public static final String BUZZ = "buzz";
+
     public static String fizzBuzz(int number){
         StringBuilder stringBuilder = new StringBuilder();
+
         if (number % 3 == 0){
-            stringBuilder.append("fizz");
+            stringBuilder.append(FIZZ);
         }
 
         if (number % 5 == 0){
-            stringBuilder.append("buzz");
+            stringBuilder.append(BUZZ);
         }
 
         return stringBuilder.length() > 0 ? stringBuilder.toString() : String.valueOf(number);
--- app/src/test/java/com/the3rocks/karumitestingcourseexerciseone/FizzBuzzTest.java
+++ app/src/test/java/com/the3rocks/karumitestingcourseexerciseone/FizzBuzzTest.java
@@ -12,6 +12,7 @@ public class FizzBuzzTest {
     public static final int ONLY_MULTIPLE_OF_THREE = 3;
     public static final int ONLY_MULTIPLE_OF_FIVE = 5;
     public static final int MULTIPLE_OF_THREE_AND_FIVE = 15;
+    public static final int NON_MULTIPLE_OF_THREE_AND_FIVE = 2;
 
     @Test
     public void Maxint_returns_maxint() throws Exception {
@@ -20,21 +21,26 @@ public class FizzBuzzTest {
 
     @Test
     public void zero_returns_fizzbuzz() throws Exception {
-        assertEquals("fizzbuzz", FizzBuzz.fizzBuzz(0));
+        assertEquals(FizzBuzz.FIZZ + FizzBuzz.BUZZ, FizzBuzz.fizzBuzz(0));
     }
 
     @Test
     public void multiple_of_three_returns_fizz() throws Exception {
-        assertEquals("fizz", FizzBuzz.fizzBuzz(ONLY_MULTIPLE_OF_THREE));
+        assertEquals(FizzBuzz.FIZZ, FizzBuzz.fizzBuzz(ONLY_MULTIPLE_OF_THREE));
     }
 
     @Test
     public void multiple_of_five_returns_buzz() throws Exception {
-        assertEquals("buzz", FizzBuzz.fizzBuzz(ONLY_MULTIPLE_OF_FIVE));
+        assertEquals(FizzBuzz.BUZZ, FizzBuzz.fizzBuzz(ONLY_MULTIPLE_OF_FIVE));
     }
 
     @Test
     public void multiple_of_three_and_five_returns_fizzbuzz() throws Exception {
-        assertEquals("fizzbuzz", FizzBuzz.fizzBuzz(MULTIPLE_OF_THREE_AND_FIVE));
+        assertEquals(FizzBuzz.FIZZ + FizzBuzz.BUZZ, FizzBuzz.fizzBuzz(MULTIPLE_OF_THREE_AND_FIVE));
+    }
+
+    @Test
+    public void non_multiple_of_three_and_five_returns_number() throws Exception {
+        assertEquals(String.valueOf(NON_MULTIPLE_OF_THREE_AND_FIVE), FizzBuzz.fizzBuzz(NON_MULTIPLE_OF_THREE_AND_FIVE));
     }
 }
